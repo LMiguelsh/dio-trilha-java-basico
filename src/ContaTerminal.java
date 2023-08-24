ab815f0f4a942b5f939eeabfe5470690c461e4d4
LMiguelsh <luis.miguel1500@gmail.com>
2023-08-24 09:58:40 -0300

    Projeto banco finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da Agencia !");
+        int numero = ler.nextInt();
+        ler.nextLine();
+        System.out.println("Por favor,informe a Agencia !");
+        String agencia = ler.nextLine();
+
+
+        System.out.println("Por favor, informe seu nome !");
+        String nomeCliente = ler.nextLine();
+
+        System.out.println("Por favor, informe seu saldo !");
+        double saldo = ler.nextDouble();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agencia "+agencia+", conta "+numero+" e seu saldo "+saldo+" esta disponivel para saque.");
     }
 }
